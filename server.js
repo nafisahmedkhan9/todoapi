f8f099260dc1b2d437bf416d09c6550c00c1cbcb
Nafis Ahmed <nafisahmedkhan9@gmail.com>
2016-11-07 13:45:40 +0530

    7/11 using underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000 ;
 var nextId = 1 ;
@@ -16,28 +17,29 @@ app.get('/data',function(req,res){
 	res.json(todos);
 });
 app.get('/data/:id',function(req,res){
-	console.log("GET Request Hit /data/:id  !");
+	console.log("GET Request Hit /data/:id !");
 	var inputid = parseInt(req.params.id) ;
-	var index;
-	for (var i = 0; i < todos.length; i++) {
-		if (inputid == todos[i].id) {
-			index = i ;
-			break;		
-		}
-	}
-	if (index != null) {
-		res.json(todos[index]);	
+	var id_data = _.findWhere(todos, {id: inputid});
+
+	if (id_data) {
+		res.json(id_data);	
 	}else{
 		res.status(404).send();
 	}
 });
 app.post('/data',function(req,res){
-	var body = req.body ;
-	console.log('GET Request HIT /data !');
+	var body = _.pick(req.body,'name','surname','nature') ;
 	body.id = nextId++;
+	
+	if (!_.isBoolean(body.nature) || !_.isString(body.name) || !_.isString(body.surname) || body.name.trim().length == 0 || body.surname.trim().length == 0 ) {
+		res.status(400).send();
+	}	
+
+	console.log('POST Request HIT /data !');
+	body.name = body.name.trim();
+	body.surname = body.surname.trim();
 	todos.push(body);
 	res.json(body);
-
 })
 app.listen(PORT,function(){
 	console.log("Express Server Is Started "+PORT+" .")
